fcb2c9580452b155752e682ddf1035dda5c185e4
Vikram728 <95694033+Vikram728@users.noreply.github.com>
2022-01-10 12:29:16 +0530

    10-1-2022
    First upload

---
+++ Basics.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+a=1
+b=2
+print("The sum of a and b is:",a+b)
+
+
+# In[ ]:
+
+
+#Add number by taking inputs from the user
+
+num1=input('Enter the first number:')
+num2=input('Enter the second number:',)
+
+num3=int(num1)+int(num2)
+
+print("The sum of both numnbers are:", num3)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
